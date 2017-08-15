91fe1b06aba7a4626747d0d95a7d7f4e692b5c65
andrii-shchudlo <andriis@quintagroup.org>
2017-08-15 18:02:30 +0300

    Created method calculate_discount_coef

--- openprocurement/tender/esco/npv_calculation.py
+++ openprocurement/tender/esco/npv_calculation.py
@@ -1,4 +1,12 @@
 
+DISCOUNT_RATE = [4.623, 12.500, 12.500, 12.500, 12.500, 12.500, 12.500, 12.500, 12.500, 12.500, 12.500,
+                 12.500, 12.500, 12.500, 12.500, 12.500, 12.500, 12.500, 12.500, 12.500, 7.877]
 
-def calculate_discount_coef():
-    pass
+def calculate_discount_coef( discount_rate = DISCOUNT_RATE):
+    discount_coef = []
+    coefficient = 1
+    for i in discount_rate:
+        res = round((float(coefficient)/(i/100+1)),3)
+        coefficient = res
+        discount_coef.append(res)
+    return discount_coef
