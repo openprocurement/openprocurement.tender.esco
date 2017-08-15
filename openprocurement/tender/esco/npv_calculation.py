88c994822b0d95f9d3ead734d733fa755aabbbee
vladkhard <vladkhard@gmail.com>
2017-08-15 17:36:35 +0300

    convert tabs into spaces

--- openprocurement/tender/esco/npv_calculation.py
+++ openprocurement/tender/esco/npv_calculation.py
@@ -5,25 +5,25 @@ def calculate_contract_duration(
         contract_duration_years,
         contract_duration_days,
         days_per_year=DAYS_PER_YEAR):
-	'''Calculate contract duration in days'''
+    '''Calculate contract duration in days'''
 
-	return contract_duration_years * days_per_year + contract_duration_days
+    return contract_duration_years * days_per_year + contract_duration_days
 
 
 def calculate_days_with_cost_reduction(
         days_per_year=DAYS_PER_YEAR,
         announcement_date=ANNOUNCEMENT_DATE):
-	
-	first_year_days = (datetime.date(announcement_date.year, 12, 31) - announcement_date).days
     
-	days = [first_year_days] + [days_per_year] * 20
-	return days
+    first_year_days = (datetime.date(announcement_date.year, 12, 31) - announcement_date).days
+
+    days = [first_year_days] + [days_per_year] * 20
+    return days
 
 
 def calculate_days_for_discount_rate(
-	days_per_year=DAYS_PER_YEAR,
-	announcement_date=ANNOUNCEMENT_DATE):
-	days = calculate_days_with_cost_reduction(days_per_year, announcement_date)[:-1]
-	days.append(days_per_year - days[0])
+        days_per_year=DAYS_PER_YEAR,
+        announcement_date=ANNOUNCEMENT_DATE):
+    days = calculate_days_with_cost_reduction(days_per_year, announcement_date)[:-1]
+    days.append(days_per_year - days[0])
 
-	return days
+    return days
