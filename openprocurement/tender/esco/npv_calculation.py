801d8270ac68e6cb2e3b9657f6542aea00ea3a81
vladkhard <vladkhard@gmail.com>
2017-08-15 16:16:18 +0300

    add calculation for discount rate

---
+++ openprocurement/tender/esco/npv_calculation.py
@@ -0,0 +1,14 @@
+announcement_date = "2017-08-18"
+DAYS_IN_YEAR = 365
+
+
+def calculate_days_for_discount_rate(announcement_date):
+    days = calculate_days_with_cost_reduction(announcement_date)[:-1]
+    days.append(DAYS_IN_YEAR - days[0])
+    return days
+
+
+
+days = calculate_days_for_discount_rate(announcement_date)
+print days
+print len(days)
