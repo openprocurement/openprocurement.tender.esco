c3e7ed53b11d8db02cc4e46d39b477a5647fc052
vladkhard <vladkhard@gmail.com>
2017-08-15 17:31:35 +0300

    change spaces to tabs

--- openprocurement/tender/esco/npv_calculation.py
+++ openprocurement/tender/esco/npv_calculation.py
@@ -5,19 +5,19 @@ def calculate_contract_duration(
         contract_duration_years,
         contract_duration_days,
         days_per_year=DAYS_PER_YEAR):
-    '''Calculate contract duration in days'''
+	'''Calculate contract duration in days'''
 
-    return contract_duration_years * days_per_year + contract_duration_days
+	return contract_duration_years * days_per_year + contract_duration_days
 
 
 def calculate_days_with_cost_reduction(
         days_per_year=DAYS_PER_YEAR,
         announcement_date=ANNOUNCEMENT_DATE):
+	
+	first_year_days = (datetime.date(announcement_date.year, 12, 31) - announcement_date).days
     
-    first_year_days = (datetime.date(announcement_date.year, 12, 31) - announcement_date).days
-    
-    days = [first_year_days] + [days_per_year] * 20
-    return days
+	days = [first_year_days] + [days_per_year] * 20
+	return days
 
 
 def calculate_days_for_discount_rate(
