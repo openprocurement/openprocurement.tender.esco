c591e217966b1c13d8eecbd5c1dbf7e7f659a21b
andrii-shchudlo <andriis@quintagroup.org>
2017-08-16 16:26:04 +0300

    Removed float type

--- openprocurement/tender/esco/npv_calculation.py
+++ openprocurement/tender/esco/npv_calculation.py
@@ -1,10 +1,10 @@
 from fractions import Fraction
-DISCOUNT_RATE = [4.623]+[12.500]*19 + [7.877]
+DISCOUNT_RATE = [Fraction(str(4.623))]+[Fraction(str(12.500))]*19 + [Fraction(str(7.877))]
 
-def calculate_discount_coef( discount_rate = DISCOUNT_RATE):
+def calculate_discount_coef(discount_rate = DISCOUNT_RATE):
     discount_coef = []
     coefficient = Fraction(1)
     for i in discount_rate:
-        coefficient = Fraction(coefficient, (Fraction(Fraction(str(i)), Fraction(100))+Fraction(1)))
-        discount_coef.append(float(coefficient))
+        coefficient = Fraction(coefficient, (Fraction(i, Fraction(100))+Fraction(1)))
+        discount_coef.append(coefficient)
     return discount_coef
