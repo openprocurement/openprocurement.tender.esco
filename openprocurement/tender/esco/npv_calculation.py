746f85662f502e298268ac230e99cc9ea9d89aba
Tuoris <tuoris.dev@gmail.com>
2017-08-16 17:01:35 +0300

    Fix PEP 8 minor error

--- openprocurement/tender/esco/npv_calculation.py
+++ openprocurement/tender/esco/npv_calculation.py
@@ -65,10 +65,10 @@ def calculate_payment(
 
 
 def calculate_payments(
-       yearly_payments_percentage,
-       client_cost_reductions,
-       days_with_payments,
-       days_for_discount_rate):
+        yearly_payments_percentage,
+        client_cost_reductions,
+        days_with_payments,
+        days_for_discount_rate):
     '''Calculates client payments to a participant'''
 
     lists = zip(
