92471aabea00f2d33c9ee15aa227bfab88441f43
andrii-shchudlo <andriis@quintagroup.org>
2017-08-16 12:18:04 +0300

    Add Fraction

--- openprocurement/tender/esco/npv_calculation.py
+++ openprocurement/tender/esco/npv_calculation.py
@@ -1,9 +1,10 @@
+from fractions import Fraction
 DISCOUNT_RATE = [4.623]+[12.500]*19 + [7.877]
 
 def calculate_discount_coef( discount_rate = DISCOUNT_RATE):
     discount_coef = []
-    coefficient = 1
+    coefficient = Fraction(1)
     for i in discount_rate:
-        coefficient = round((float(coefficient)/(i/100+1)), 3)
-        discount_coef.append(coefficient)
+        coefficient = Fraction(coefficient, (Fraction(Fraction(str(i)), Fraction(100))+Fraction(1)))
+        discount_coef.append(float(coefficient))
     return discount_coef
