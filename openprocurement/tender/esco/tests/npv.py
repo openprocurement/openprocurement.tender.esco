97b26ff1a361eb2b702bf1e8f0072fd960bc73d5
andrii-shchudlo <andriis@quintagroup.org>
2017-08-17 15:54:25 +0300

    Add tests

--- openprocurement/tender/esco/tests/npv.py
+++ openprocurement/tender/esco/tests/npv.py
@@ -1,8 +1,11 @@
 import unittest
 from openprocurement.api.tests.base import snitch
-
 from openprocurement.tender.esco.tests.npv_blanks import (
-    npv_case1, npv_case2, npv_case3, npv_case4
+    discount_coef,
+    contract_duration,
+    discount_rate,
+    discount_rates,
+    days_with_cost_reduction,
 )
 
 
@@ -10,11 +13,11 @@ class NPVCalculationTest(unittest.TestCase):
     """ NPV Calculation Test
         based on data from https://docs.google.com/spreadsheets/d/1kOz6bxob4Nmb0Es_W0TmbNznoYDcnwAKcSgxfPEXYGQ/edit#gid=1469973930
     """
-
-    test_case1 = snitch(npv_case1)
-    test_case2 = snitch(npv_case2)
-    test_case3 = snitch(npv_case3)
-    test_case4 = snitch(npv_case4)
+    test_discount_coef = snitch(discount_coef)
+    test_contract_duration = snitch(contract_duration)
+    test_discount_rate = snitch(discount_rate)
+    test_discount_rates = snitch(discount_rates)
+    test_days_with_cost_reduction = snitch(days_with_cost_reduction)
 
 
 def suite():
